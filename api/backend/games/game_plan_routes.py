ee29a092ad0eda462f14ac83889c8fef1c57dfc0
frankhealey3@gmail.com <dankowskihealey.f@northeastern.edu>
2025-08-11 19:37:23 -0400

    Updated gameplan routes

--- api/backend/games/game_plan_routes.py
+++ api/backend/games/game_plan_routes.py
@@ -2,13 +2,12 @@
 from flask import Blueprint, request, jsonify, make_response, current_app, redirect, url_for
 import json
 from backend.db_connection import db
-from backend.simple.playlist import sample_playlist_data
 
-game_plan_routes = Blueprint('game_plan_routes', __name__)
+gameplans = Blueprint('gameplans', __name__)
 
 # ------------------------------------------------------------
 # /gameplans returns all rows from the GamePlan table
-@game_plan_routes.route('/gameplans', methods=['GET'])
+@gameplans.route('/gameplans', methods=['GET'])
 def get_all_gameplans():
     current_app.logger.info('GET /gameplans handler')
     
