b1dac6faba2d54f89de6503da89430215c8ffebd
andrewdubanowitz <dubanowitz.a@northeastern.edu>
2025-08-11 18:47:44 -0400

    created a folder for gm, and correspondig subpages

---
+++ app/src/pages/general_manager/31_Player_Valuation.py
@@ -0,0 +1,10 @@
+logger = logging.getLogger(__name__)
+
+import streamlit as st
+from modules.nav import SideBarLinks
+
+st.set_page_config(layout='wide')
+SideBarLinks()
+st.title('Player Valuation')
+st.write("This page will be used to assess player value and potential.")
+# Add your player valuation code here
---
+++ app/src/pages/general_manager/32_Draft_Analysis.py
@@ -0,0 +1,12 @@
+logger = logging.getLogger(__name__)
+
+import streamlit as st
+from modules.nav import SideBarLinks
+
+st.set_page_config(layout='wide')
+SideBarLinks()
+st.title('Draft Analysis')
+st.write("This page will provide tools for draft analysis and player evaluation.")
+# Add your draft analysis code here
+
+
